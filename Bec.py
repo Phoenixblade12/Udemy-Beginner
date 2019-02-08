b0439e425e6b0cdea9054e5d7f0ede8f01101485
Phoenixblade12 <47441668+Phoenixblade12@users.noreply.github.com>
2019-02-08 15:36:37 +1000

    Add files via upload
    Uploaded first file

---
+++ Bec.py
@@ -0,0 +1,6 @@
+print("Hello, my name is Bec Alesech")
+name = input("what's your name? ")
+if name == "Phoenix":
+    print("Wow, that's a great name",name)
+else:
+    print ("hmmm...that's an odd name!")
